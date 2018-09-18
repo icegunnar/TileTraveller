79cd348e3060cc7193fe909f5d3eff55aa9b4688
Gunnar Björn Helgason <gunnarh17@ru.is>
2018-09-18 11:40:56 +0000

    init

---
+++ tiletraveller.py
@@ -0,0 +1,39 @@
+
+#bý til upphafsstöðu
+x_pos = 1
+y_pos = 1
+
+#frá upphafsstöðu er bara hægt að ferðast norður.
+direction = input('You can travel: (N)orth')
+if direction == 'n':
+    x_pos = 1
+    y_pos = 2
+else:
+    print('invalid input')
+
+direction = input('You can travel: (N)orth, (S)outh or (E)ast')
+if direction == 'n':
+    x_pos = 1
+    y_pos = 3
+elif direction == 's':
+    x_pos = 1
+    y_pos = 1
+elif direction == 'e':
+    x_pos = 2
+    y_pos = 2
+else:
+    print('invalid input')
+
+while pos_x != 3 and pos_y != 3
+
+
+
+
+        
+
+
+
+
+
+
+
